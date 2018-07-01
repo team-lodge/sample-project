16ae5e1aaa4a5acb44cd3b2f747a17bc0bc54825
kawa1228 <sxa.1105bo@gmail.com>
2018-07-01 15:01:43 +0900

    まだ途中

--- main.js
+++ main.js
@@ -1,63 +1,69 @@
-function errorMessage(){
-    const form = document.form1;
-    const names = form.names.value;
+function onButtonClick(){
+  var names = document.form1.names.value;
+  var num = document.form1.num.value;
 
-    const data ={};
-   console.log(names);
-   console.log(names.length);
-   
-   
+  var list = names.split(',')
 
-   // const message = 'error'
-   // if(names.length < num.length){
-   //     // console.log(message);
-   //     alert(message);
-   // }
-   // return message
- }
+  if(num < list.length){
+    return true;
+  } else {
+    return false;
+  }
+}
 
- /**
-  * メンバーを抽選する1
-  */
- function chooseMember() {
-   errorMessage();
-   const form = document.form1;
-   const names = form.names.value;
-   const list = names.split(',');
+/**
+* メンバーを抽選する1
+*/
+function chooseMember() {
+  
+  const form = document.form1;
+  const names = form.names.value;
+  const list = names.split(',');
 
-   const count = form.num.value;
-   let winners = [];
+  const count = form.num.value;
+  let winners = [];
 
-   // 指定された当選者数分だけ for で回す
-   for (let i = 0; i < count; i++) {
-     /**
-      * 乱数の取得に関して、このサイトがわかりやすいです。
-      * https://www.sejuku.net/blog/22432
-      */
-     const chosen_index = Math.floor(Math.random() * list.length);
-     // 当選者を当選者用配列に詰める
-     winners.push(list[chosen_index]);
-    
-     // 応募者リストから、当選したメンバーを除外する
-     list.splice(chosen_index, 1);
-   }
-   output(winners);
- }
+  // 指定された当選者数分だけ for で回す
+  for (let i = 0; i < count; i++) {
+    /**
+    * 乱数の取得に関して、このサイトがわかりやすいです。
+    * https://www.sejuku.net/blog/22432
+    */
+    const chosen_index = Math.floor(Math.random() * list.length);
+    // 当選者を当選者用配列に詰める
+    winners.push(list[chosen_index]);
+  
+    // 応募者リストから、当選したメンバーを除外する
+    list.splice(chosen_index, 1);
+  }
+  output(winners);
+}
 
- /**
-  * HTML タグを生成して出力する
-  * @param {string[]} winners 当選者の配列
-  */
- function output(winners) {
-   let elem = document.getElementById('js-list');
-   elem.innerText = '';
-   let ul = document.createElement('ul');
-   for (let i = 0; i < winners.length; i++) {
-     let li = document.createElement('li');
-     li.innerText = winners[i]+' さん';
-     ul.appendChild(li);
-   }
+/**
+* HTML タグを生成して出力する
+* @param {string[]} winners 当選者の配列
+*/
+function output(winners) {
+  let elem = document.getElementById('js-list');
+  elem.innerText = '';
+  let ul = document.createElement('ul');
+  for (let i = 0; i < winners.length; i++) {
+    let li = document.createElement('li');
+    li.innerText = winners[i]+' さん';
+    ul.appendChild(li);
+  }
 
-   elem.appendChild(ul);
- }
+  elem.appendChild(ul);
+}
 
+var hoge = document.querySelector('.hoge')
+
+hoge.addEventListener('click', ()=>{
+
+  if(onButtonClick() === false){
+    alert('当選者数が多すぎます')
+  } else {
+
+
+  }
+})
